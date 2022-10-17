817ab9430ba41a78266474b803cd67a3b8260c61
Igor Tarasenko <igtarako@gmail.com>
2022-10-17 15:11:50 +0700

    init

---
+++ internal/tracer/tracer.go
@@ -0,0 +1,35 @@
+package tracer
+
+import (
+	"io"
+
+	"github.com/ITarako/coshkey_tree/internal/config"
+	"github.com/rs/zerolog/log"
+
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+)
+
+// NewTracer - returns new tracer.
+func NewTracer(cfg *config.Config) (io.Closer, error) {
+	cfgTracer := &jaegercfg.Configuration{
+		ServiceName: cfg.Jaeger.Service,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: cfg.Jaeger.Host + cfg.Jaeger.Port,
+		},
+	}
+	tracer, closer, err := cfgTracer.NewTracer(jaegercfg.Logger(jaeger.StdLogger))
+	if err != nil {
+		log.Err(err).Msgf("failed init jaeger: %v", err)
+
+		return nil, err
+	}
+	opentracing.SetGlobalTracer(tracer)
+	log.Info().Msgf("Traces started")
+
+	return closer, nil
+}
